08d94e1bc560999a5df9f71ede268c3178fa0b0a
Greyson H. Bourgeois <bourgeoi1@cs.grinnell.edu>
2018-05-07 09:35:17 -0500

    fixed by a rewrite. still needs to be commented

--- pfs.c
+++ pfs.c
@@ -11,79 +11,79 @@
 
 //--------------------------------------------------------
 typedef struct to_recurse {
-	char *file_name;
-	struct to_recurse *next;
+  char *file_name;
+  struct to_recurse *next;
 } to_recurse_t;
 
 
 typedef struct queue {
-	to_recurse_t *front;
-	to_recurse_t *back;
+  to_recurse_t *front;
+  to_recurse_t *back;
 }queue_t;
 
 //Set up the to_recurse_t structs (queue nodes)
 to_recurse_t* new_to_recurse(char* file_name) {
-	to_recurse_t *new = malloc(sizeof(to_recurse_t));
-	// new->file_name = malloc(sizeof(char)*500);
-	new->file_name = file_name;
-	//strcpy(new->file_name, file_name);
-	new->next = NULL;
-	return new;
+  to_recurse_t *new = malloc(sizeof(to_recurse_t));
+  // new->file_name = malloc(sizeof(char)*500);
+  new->file_name = file_name;
+  //strcpy(new->file_name, file_name);
+  new->next = NULL;
+  return new;
 }
 
 //Set up a queue
 queue_t* init_queue(){
-	queue_t* ret = malloc(sizeof(queue_t));
-	ret->front = NULL;
-	ret->back = NULL;
-	return ret;
+  queue_t* ret = malloc(sizeof(queue_t));
+  ret->front = NULL;
+  ret->back = NULL;
+  return ret;
 }
 
 //Add a string to a node in the queue
 void add_to_queue(queue_t *queue, char* file_name){
-	to_recurse_t *new = new_to_recurse(file_name); 
-	if(queue->front == NULL){
-		printf("First Element in queue\n");
-		queue->front = new;
-		queue->back = new;
-	} else {
-		printf("Last element in queue is %s\n", queue->back->file_name);
-		queue->back->next = new;
-		queue->back = queue->back->next;
-		queue->back->next = NULL;
-	}
-	printf("Queue front is %s\n", queue->front->file_name);
-	printf("Queue back is %s\n", queue->back->file_name);
+  to_recurse_t *new = new_to_recurse(file_name); 
+  if(queue->front == NULL){
+    //printf("First Element in queue\n");
+    queue->front = new;
+    queue->back = new;
+  } else {
+    //printf("Last element in queue is %s\n", queue->back->file_name);
+    queue->back->next = new;
+    queue->back = queue->back->next;
+    queue->back->next = NULL;
+  }
+  //printf("Queue front is %s\n", queue->front->file_name);
+  //printf("Queue back is %s\n", queue->back->file_name);
 }
 
 //Get the next element in the queue
 to_recurse_t* get_next(queue_t *queue){
-	to_recurse_t* ret = queue->front;
-	if(ret != NULL){
-		queue->front = queue->front->next;
-	}
-	return ret;
+  to_recurse_t* ret = queue->front;
+  if(ret != NULL){
+    queue->front = ret->next;
+  }
+  return ret;
 }
 
 //Free the queue
 void free_queue(queue_t *queue){
-	to_recurse_t* cur = queue->front;
-	to_recurse_t* next = queue->front;
-	while(cur != NULL){
-		next = cur->next;
-		free(cur);
-		cur = next;
-	}
-	free(queue);
+  to_recurse_t* cur = queue->front;
+  to_recurse_t* next = queue->front;
+  while(cur != NULL){
+    next = cur->next;
+    free(cur);
+    cur = next;
+  }
+  free(queue);
 }
 //Print the queue
 void print_queue(queue_t* queue){
-	to_recurse_t* cur = queue->front;
-	printf("Queue Elements:\n");
-	while (cur != NULL){
-		printf("\t%s\n", cur->file_name);
-		cur = cur->next;
-	}
+  to_recurse_t* cur = queue->front;
+  printf("Queue Elements:\n");
+  while (cur != NULL){
+    printf("\t%s\n", cur->file_name);
+    cur = cur->next;
+  }
 }
 //----------------------------------------------------------------
 queue_t* queue; //The backlog queue
@@ -96,202 +96,110 @@ int cur_threads; //# of threads running
 
 //Args for each search thread
 typedef struct thread_args{
-	char* file_name;
+  char* file_name;
 }thread_args_t;
 
 char* search_str;
 
+#define MAX_NAME 500
 
 //Threaded searching function
 void* search_dir(void* args){
-	//Unpack args
-	thread_args_t* thread_args = (thread_args_t*) args;
-	// char* dir_name = malloc(sizeof(char)*500);
-	//strcpy(dir_name, thread_args-> file_name);
-	//free(args);
-	char*dir_name = thread_args->file_name;
-
+  pthread_t new_thread[max_threads];
+  thread_args_t* dir_thread_args[max_threads];
+  int cur_thread = 0;
   
-	printf("Searching : %s\n", dir_name);
+  thread_args_t *arg = (thread_args_t*) args;
+  DIR* cur_dir = opendir(arg->file_name);
+  if (cur_dir == NULL){
+    fprintf(stderr,"Unable to open directory %s \n", arg->file_name);
+    exit (EXIT_FAILURE);
+  }
+  struct stat file_stat;
 
-	//Open a directory from the file name
-	DIR* cur_dir = opendir(dir_name);
-	if (cur_dir == NULL){
-		fprintf(stderr,"Unable to open directory %s \n", dir_name);
-		exit (EXIT_FAILURE);
-	}
-	//Get a directory entry  
-	struct dirent* cur_file = (struct dirent*)malloc(sizeof(struct dirent));
-	//readdir(cur_dir);
-	cur_file = readdir(cur_dir);
+  struct dirent* cur_file = readdir(cur_dir);
+  while(cur_file != NULL){
+    if(!((strcmp(cur_file->d_name, arg->file_name) == 0)||
+         (strcmp(cur_file->d_name, "..") == 0) ||
+         (strcmp(cur_file->d_name, ".") == 0))){
+      if(strstr(cur_file->d_name, search_str) != NULL){printf("%s in %s\n", cur_file->d_name, arg->file_name);}
+      char* cur_path = malloc(sizeof(char)*MAX_NAME);
+      strcpy(cur_path, arg->file_name);
+      strcat(cur_path, "/");
+      strcat(cur_path, cur_file->d_name);
+      
+      if(stat(cur_path, &file_stat) != 0){
+        fprintf(stderr,"Stat failed: %s\n", cur_path);
+        exit(2);
+      }
+      if (S_ISDIR(file_stat.st_mode)){
+        //printf("%s is a directory\n", cur_file->d_name);
+        pthread_mutex_lock(&count_lock);
+        if (cur_threads >= max_threads){
+          pthread_mutex_lock(&q_lock);
+          add_to_queue (queue, cur_path);
+          pthread_mutex_unlock(&q_lock);
+        } else {
+          dir_thread_args[cur_thread]  = malloc(sizeof(thread_args_t));
+          dir_thread_args[cur_thread]->file_name = cur_path;
+          pthread_create(&(new_thread[cur_thread]), NULL, search_dir, dir_thread_args[cur_thread]);
+          cur_threads ++;
+          cur_thread ++;
+        }
+        pthread_mutex_unlock(&count_lock);
+      }
+    }
+    cur_file = readdir(cur_dir);
+  }
+  closedir(cur_dir);
+
+  pthread_mutex_lock(&q_lock);
+  to_recurse_t* next_dir = get_next(queue);
+  pthread_mutex_unlock(&q_lock);
+  if(next_dir != NULL){
+    thread_args_t* next_args = malloc(sizeof(thread_args_t));
+    next_args->file_name = next_dir->file_name;
+    search_dir(next_args);
+    free(next_args);
+    free(next_dir->file_name);
+    free(next_dir);
+  }
+
+  //printf("ENDING_THREAD\n");
+  pthread_mutex_lock(&count_lock);
+  cur_threads --;
+  pthread_mutex_unlock(&count_lock);
+
+  return NULL;
     
-	//Access and store file metadata
-	struct stat file_stat;
 
-
-	//Loop our way through the directory
-	while(cur_file != NULL){
-		//Prevent searching in .. 
-		if(!((strcmp(cur_file->d_name, dir_name) == 0)||
-			(strcmp(cur_file->d_name, "..") == 0) ||
-		(strcmp(cur_file->d_name, ".") == 0) )){
-                  
-			//Create the file path
-			char cur_path[500];
-			strcpy(cur_path, dir_name);
-			strcat(cur_path, "/");
-			strcat(cur_path, cur_file->d_name);
-			//printf("%s\n", cur_path);
-	  
-			//Store the info about the file in a stat
-			if(stat(cur_path, &file_stat) != 0){
-				fprintf(stderr,"Stat failed: %s\n", cur_path);
-				exit(2);
-			}
-		
-			//Check if the current file/directory name contains the
-			//search string
-      
-			//printf ("Current entry is %s in %s\n", cur_file->d_name, dir_name);
-			/*
-			if (strstr (cur_file->d_name,search_str) != NULL){
-			//Just print the file name for now.
-			printf("%s ***********************\n", cur_file->d_name); 
-			}
-			*/
-			//If the element is a directory do the thread check
-
-			if (S_ISDIR(file_stat.st_mode)){
-				printf("%s is a directory\n", cur_file->d_name);
-				//Lock the count
-				pthread_mutex_lock (&count_lock);
-
-
-				if (cur_threads >= max_threads){
-					//printf("Need to add %s to queue\n", cur_path);
-					//Lock the queue
-					pthread_mutex_lock (&q_lock);
-
-					//Add file name to queue
-					add_to_queue (queue, cur_path);
-					printf("Put %s on queue\n", cur_path);
-					print_queue(queue);
-					//Unlock queue
-					pthread_mutex_unlock (&q_lock);
-				} else{
-          
-					//Create a new thread and execute the search function
-					pthread_t dir_thread;
-      
-					thread_args_t* dir_thread_args = malloc(sizeof(thread_args_t));
-
-					// strcpy(dir_thread_args-> file_name, cur_path);
-
-					dir_thread_args->file_name = cur_path;
-					printf("New thread to search %s\n", cur_path);
-          
-					//Create the new thread to run search_dir
-					pthread_create (&dir_thread, NULL, search_dir, dir_thread_args);
-					cur_threads++;
-				}
-				printf("%d Threads Running\n", cur_threads);
-				//Unlock the count
-				pthread_mutex_unlock (&count_lock);
-			}
-
-	  
-		} //Closing the big if
- 
-		// Continue on our way through the directory
-		cur_file = readdir(cur_dir);	
-	} //Closing the while loop
-	free(cur_file);
-
-	//Close the current directory when done
-	printf("------Closing %s-------\n", dir_name);
-	closedir(cur_dir);
-	//free(dir_name);
-
-	//Check if the queue is empty
-	pthread_mutex_lock(&q_lock);
-	to_recurse_t* next_dir = get_next(queue);
-	
-	//Check if the next thing is actually a directory...
-	struct stat next_stat;
-	while (next_dir != NULL){
-		printf("Checking : %s\n", next_dir->file_name);
-		if(stat(next_dir->file_name, &next_stat) != 0){
-			fprintf(stderr,"Stat failed");
-			exit(1);
-		}
-		if (S_ISDIR(file_stat.st_mode)){
-			break;
-		}
-		next_dir = get_next(queue);
-	}
-	
-	//Testing prints
-	if (next_dir != NULL){
-		printf("next up is : %s\n", next_dir->file_name);
-	}else{
-		printf("next_dir is NULL\n");
-	}
-	if (queue->front !=NULL){
-		printf("On deck in queue: %s\n", queue->front->file_name);
-	}
-	
-	//Unlock the queue
-	pthread_mutex_unlock(&q_lock);
-
-	//if next_dir is not null
-	if(next_dir != NULL){
-		thread_args_t* next_args = malloc(sizeof(thread_args_t));
-		next_args->file_name = next_dir->file_name;
-		//strcpy(next_args->file_name, next_dir->file_name);
-		printf("got %s from queue\n", next_args->file_name);
-		search_dir(next_args);
-
-		//free(next_dir->file_name);
-		free(next_args);
-		free(next_dir);
-	}
-	//pthread_mutex_unlock(&q_lock);
-	printf("The Queue is empty\n");
-	//Decrement the number of threads running
-	pthread_mutex_lock(&count_lock);
-	cur_threads --;
-	pthread_mutex_unlock(&count_lock);
-
-	return NULL;
 }
 
-//Set up and run the search
 void start_search(char* file_name, char* str){
 
-	//init stuff
-	queue = init_queue();
+  //init stuff
+  queue = init_queue();
 
-	//add in code to get num of cpus
-	max_threads = 1;
-	//max_threads = 2 * sysconf(_SC_NPROCESSORS_ONLN); 
-	printf("Max threads: %d\n", max_threads);
+  //add in code to get num of cpus
+  max_threads = 1;
+  //max_threads = 2 * sysconf(_SC_NPROCESSORS_ONLN); 
+  printf("Max threads: %d\n", max_threads);
 	
-	cur_threads = 1;
+  cur_threads = 1;
 
-	//Init the locks
-	pthread_mutex_init(&q_lock, NULL);
-	pthread_mutex_init(&count_lock, NULL);
+  //Init the locks
+  pthread_mutex_init(&q_lock, NULL);
+  pthread_mutex_init(&count_lock, NULL);
   
-	//Initialize search string global
-	search_str = str;
+  //Initialize search string global
+  search_str = str;
   
-	thread_args_t* args = malloc (sizeof (thread_args_t));
-	args->file_name = file_name;
+  thread_args_t* args = malloc (sizeof (thread_args_t));
+  args->file_name = file_name;
 	
-	search_dir (args);
+  search_dir (args);
 
 }
-
 //------------------------------------------------------
 //Main function
 int main(int argc, char** argv){
@@ -305,4 +213,3 @@ int main(int argc, char** argv){
 	
 	return 0;
 }
-
