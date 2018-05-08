ce6c77e7326d5272c440613187d449dc162e3927
Nolan Schoenle <schoenle@grinnell.edu>
2018-05-08 09:59:28 -0500

    Add documentation to tests

--- test.c
+++ test.c
@@ -15,27 +15,36 @@
   
 int main(int argc, char** argv){
 
-  
+  //Take in the command line argument for the search term  
   if(argc != 2){
     fprintf(stderr, "Usage: %s <search term>\n", argv[0]);
     exit(EXIT_FAILURE);
   }
 
   
-  // printf("In main\n");
-  int one = 0;
+  //The time counters
+
+  //Multiples of processors
+  int one = 0; 
   int two = 0;
   int four = 0;
   int eight = 0;
-  int linear = 0;
   int sixteen = 0;
   int thirty_two = 0;
   int sixty_four = 0;
-  
+
+  //One thread
+  int linear = 0;
+
+  //Run the test 50 times
   for(int i = 0; i < 50; i++){
-    
+    //Get the time
     clock_t start = clock();
+
+    //Do a search from the current directory
     start_l_search(".", argv[1]);
+
+    //Get the time elapsed
     clock_t diff = clock() - start;
     one += diff * 1000 / CLOCKS_PER_SEC;
     /* start = clock(); */
@@ -68,6 +77,7 @@ int main(int argc, char** argv){
     /* linear += diff * 1000 / CLOCKS_PER_SEC; */
     
   }
+  //Print the results to the terminal
   printf("one: %d\n", one/50);
   printf("two: %d\n", two/50);
   printf("four: %d\n", four/50);
@@ -77,7 +87,5 @@ int main(int argc, char** argv){
   printf("sixty_four: %d\n", sixty_four/50);
   printf("linear: %d\n", linear/50);
 
-
-  //Start the search in the current directory
   return 0;
 }
